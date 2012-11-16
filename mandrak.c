eeab2f6619fe9e79520d8eb7f5ad99066b98fa36
Sander Demeester <demeester.sander@gmail.com>
2012-11-16 22:07:46 +0100

    added header files

--- mandrak.c
+++ mandrak.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <pthread.h>
+
 #include "mandrak.h"
+
 
 int main(int argc, char **argv){
   return 0;
