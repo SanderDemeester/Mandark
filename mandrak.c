e2134405d8c673114200c5ec5d3398c84341b86a
Sander Demeester <demeester.sander@gmail.com>
2012-11-17 17:07:15 +0100

    Check if provided device is up and get protocol family + adr

--- mandrak.c
+++ mandrak.c
@@ -6,6 +6,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/types.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
 #include <pthread.h>
 
 #include "mandrak.h"
@@ -18,7 +20,7 @@ int main(int argc, char **argv){
 
 void parse_arguments(int argc, char **argv){
   if(argc <= 3){
-    help(argc,argv);
+    help(argc,argv,1);
   }
     
   int port_flag = 0;
@@ -33,31 +35,61 @@ void parse_arguments(int argc, char **argv){
   arguments *arg = (arguments*) malloc(sizeof(arguments));
   
   int opterr = 0;
-  while((c = getopt(argc, argv,"d:p:i:v")) != -1){
+  while((c = getopt(argc, argv,"d:p:i:vh")) != -1){
     switch(c){
     case 'd':
       ip_addr_flag = 1;
       dest_ip = optarg;
       break;
     case 'p':
-      port_flag = 0;
+      port_flag = 1;
       dest_port = optarg;
       break;
-    case 'i':
-      int_flag = 1;
-      int_f = optarg;
+    case 'i':{
+      int f_device;
+      struct ifreq netdevice; //low level acces to network devices
+      f_device = socket(AF_INET,SOCK_DGRAM,0);
+      netdevice.ifr_addr.sa_family = AF_INET;
+      strncpy(netdevice.ifr_name,optarg,IFNAMSIZ-1);
+
+      //Get interface adr for protocol family
+      if(ioctl(f_device, SIOCGIFADDR,&netdevice)){ 
+	printf("Device not found\n");
+	help(argc, argv,0);
+      }
+      close(f_device);
+      arg->if_adr = (struct sockaddr_in*) malloc(sizeof(struct sockaddr_in));
+      arg->if_adr->sin_family = AF_INET;
+      arg->if_adr->sin_addr = ((struct sockaddr_in*)&netdevice.ifr_addr)->sin_addr;
       break;
     }
+    case 'v':
+      verbose_flag = 1;
+      break;
+    case 'h':
+      help(argc,argv,1);
+      break;
+    default:
+      help(argc,argv,0);
+    }
   }
+  
   printf("%d \n", port_flag);
   printf("%d \n", ip_addr_flag);
   printf("%d \n", int_flag);
   printf("%d \n", verbose_flag);
+  
+  
+  
+  
+
   free(arg);
 }
 
-void help(int argc, char **argv){
-  printf("Usage: %s \n", argv[0]);
+void help(int argc, char **argv, int h){
+  if(h!=0)   
+    printf("Mandrak |<Copyleft>| Sander Demeester <sanderd@zeus.ugent.be>\n");
+  printf("Usage: %s -d <destination ip> -p <port> -i <network interface> [-v <verbose>]\n", argv[0]);
   
   exit(-1);
 }
--- mandrak.h
+++ mandrak.h
@@ -1,8 +1,8 @@
 #define MANDRAK_HEADER
 typedef struct{
   int port;
-  struct in_addr dest_ip; 
-  struct sockaddr_in if_adr;
+  struct in_addr *dest_ip; 
+  struct sockaddr_in *if_adr;
 }arguments;
-void help(int argc, char** argv); //function to display help page
+void help(int argc, char** argv,int h); //function to display help page
 void parse_arguments(int argc, char **argv);
